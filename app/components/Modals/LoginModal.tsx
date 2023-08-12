d53019857759ca2c40d12c88c85b862fae1d5dbb
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2023-08-13 01:05:41 +0300

    Step1 of rent property process: Category Selection

--- app/components/Modals/LoginModal.tsx
+++ app/components/Modals/LoginModal.tsx
@@ -3,7 +3,7 @@ import { signIn } from "next-auth/react";
 import axios from "axios";
 import { AiFillGithub } from "react-icons/ai";
 import { FcGoogle } from "react-icons/fc";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import Modal from "./Modal";
 import Heading from "../Heading";
@@ -75,6 +75,11 @@ const LoginModal = () => {
     </div>
   );
 
+  const toggle = useCallback(() => {
+    loginModal.onClose();
+    registerModal.onOpen();
+  }, [loginModal, registerModal]);
+
   const footercontent = (
     <div className="flex flex-col gap-4 mt-3">
       <hr />
@@ -92,12 +97,12 @@ const LoginModal = () => {
       />
       <div className="text-neutral-500 text-center mt-4 font-light">
         <div className="flex flex-row items-center justify-center gap-2">
-          <div>Already have an account?</div>
+          <div>First time using Airbnb?</div>
           <div
-            onClick={loginModal.onClose}
+            onClick={toggle}
             className="text-neutral-800 cursor-pointer hover:underline"
           >
-            Login
+            Create an account
           </div>
         </div>
       </div>
