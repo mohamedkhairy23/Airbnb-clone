733c8f7223bdd696763da8d7d5e9e23aaee4de64
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2023-08-22 18:08:00 +0300

    commit

--- app/libs/prismadb.ts
+++ app/libs/prismadb.ts
@@ -1,11 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
+declare global {
+  var prisma: PrismaClient | undefined;
+}
 
-const prisma = globalForPrisma.prisma ?? new PrismaClient();
+const client = globalThis.prisma || new PrismaClient();
+if (process.env.NODE_ENV !== "production") globalThis.prisma = client;
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
-export default prisma;
+export default client;
